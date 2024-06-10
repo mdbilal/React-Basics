24e96e9ff0a4a5ae3533ba752d89ed8550b8b68c
mdbilal <mdbilalofficial@gmail.com>
2024-06-11 01:32:04 +0500

    created header component

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Website</h1>
+      <nav>
+        <ul>
+            <li href="#">Home</li>
+            <li href="#">About Us</li>
+            <li href="#">Contact</li>
+            <li href="#">Login</li>
+        </ul>
+      </nav>
+      <hr />
+    </header>
+  );
+}
+
+export default Header;
