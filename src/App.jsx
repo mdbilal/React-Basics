1b851a9f7bce82497c19353851212ed0b127c1a8
mdbilal <mdbilalofficial@gmail.com>
2024-06-11 01:42:02 +0500

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Header from './Header.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      <p>Welcome to my website!</p>
-    </div>
+      <Footer />
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ function Header() {
       <h1>My Website</h1>
       <nav>
         <ul>
-            <li href="#">Home</li>
-            <li href="#">About Us</li>
-            <li href="#">Contact</li>
-            <li href="#">Login</li>
+          <li href="#">Home</li>
+          <li href="#">About Us</li>
+          <li href="#">Contact</li>
+          <li href="#">Login</li>
         </ul>
       </nav>
       <hr />
