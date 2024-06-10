0d10931580b305e36418ddd1403aac0905246049
mdbilal <mdbilalofficial@gmail.com>
2024-06-11 01:48:27 +0500

    added food component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
+import Food from './components/Food.jsx';
 
 function App() {
   return (
     <>
       <Header />
+      <Food />
       <Footer />
     </>
   );
---
+++ src/components/Food.jsx
@@ -0,0 +1,17 @@
+function Food(){
+    const food1 = 'Orange';
+    const food2 = 'Banana';
+    return(
+        <>
+            <h1>Food Items</h1>
+            <ul>
+                <li>Apple</li>
+                <li>{food1}</li>
+                <li>{food2}</li>
+            </ul>
+            <hr />
+        </>
+    )
+}
+
+export default Food;
