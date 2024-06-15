f96fbd4b002983392c25e55389c3763638664dc2
mdbilal <mdbilalofficial@gmail.com>
2024-06-16 02:29:04 +0500

    remove from array

--- src/components/ArrayState.jsx
+++ src/components/ArrayState.jsx
@@ -10,11 +10,15 @@ function ArrayState(){
         setArray( prevArray => [...prevArray , item]);
     }
 
+    const deleteItem = (index) => {
+        setArray(array.filter((_, i) => index != i))
+    }
+
     return (
         <>
             <h2>List of fruits</h2>
             <ul>
-                {array.map((fruit, index) => <li key={index}>{fruit}</li>)}
+                {array.map((fruit, index) => <li onClick={() => deleteItem(index)} key={index}>{fruit}</li>)}
             </ul>
             <input type="text" id="fruits" placeholder="add fruit"/>
             <button onClick={addItem}>Add</button>
