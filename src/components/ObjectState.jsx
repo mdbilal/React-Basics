3612dca70524f03fb27e3047d87fdeb76fc36714
mdbilal <mdbilalofficial@gmail.com>
2024-06-15 18:48:58 +0500

    used updater function in object State

--- src/components/ObjectState.jsx
+++ src/components/ObjectState.jsx
@@ -3,17 +3,17 @@ import { useState } from "react";
 function ObjectState(){
     const [obj, setObj] = useState({make: "honda", model: 2024, variant: "civic"});
     const handleMake = (event) => {
-        setObj({...obj, make: event.target.value})
+        setObj( preObj => ({...preObj, make: event.target.value}))
     }
     const handleModel = (event) => {
-        setObj({...obj, model: event.target.value})
+        setObj(preObj => ({...preObj, model: event.target.value}))
     }
     const handleVariant = (event) => {
-        setObj({...obj, variant: event.target.value})
+        setObj(preObj => ({...preObj, variant: event.target.value}))
     }
     return (
         <>
-            <p>My Favourite Car {obj.make} {obj.model} {obj.variant}</p>
+            <p>My Favourite Car's Make: {obj.make}, Model: {obj.model}, Variant: {obj.variant}</p>
             <input type="text" onChange={handleMake}/>
             <input type="number" onChange={handleModel}/>
             <input type="text" onChange={handleVariant}/>
